64dac020f1a720dae5ded100d7396e553a70f2a8
amin <min.hallouli@gmail.com>
2022-12-12 10:57:10 +0100

    first commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "localhost",
-  USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb",
+  USER: "testuser",
+  PASSWORD: "test",
+  DB: "testdb2",
   dialect: "mysql",
   pool: {
     max: 5,
